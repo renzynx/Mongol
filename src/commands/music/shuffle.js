a30bbd41252b62df9c314b48a1cdb41430debb64
renzynx <69468436+renzynx@users.noreply.github.com>
2021-12-07 12:31:54 +0700

    Update shuffle.js

--- src/commands/music/shuffle.js
+++ src/commands/music/shuffle.js
@@ -12,28 +12,15 @@ module.exports = class ShuffleQueue extends Command {
       category: 'Music',
       clientPermissions: ['CONNECT', 'SPEAK'],
       inVoiceChannel: true,
+      sameVoiceChannel: true,
       requiredArgs: false,
     });
   }
   async exec(message) {
     const player = this.client.manager.players.get(message.guild.id);
 
-    const channel = message.member.voice.channel;
-
-    const bot = message.guild.me.voice.channel;
-
-    if (!channel)
-      return message.channel.send(
-        '> You need to be in a voice channel to use this command.'
-      );
-
-    if (channel.id !== bot.id)
-      return message.channel.send(
-        '> You are not in the same voice channel as mine to use this command.'
-      );
-
     if (!player)
-      return message.channel.send('> There is no music playing in the guild.');
+      return message.channel.send('> There is no music playing in this guild.');
 
     player.queue.shuffle();
     return message.channel.send('> Shuffled the queue.');
