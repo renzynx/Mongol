0f1b5692892990fa1e7539b01e60df5698c873bd
renzynx <69468436+renzynx@users.noreply.github.com>
2021-12-07 10:22:43 +0700

    shuffle command

---
+++ src/commands/music/shuffle.js
@@ -0,0 +1,41 @@
+const Command = require('../../struct/Command');
+
+module.exports = class ShuffleQueue extends Command {
+  constructor() {
+    super({
+      id: 'shuffle',
+      description: 'Shuffle the current queue',
+      aliases: ['random'],
+      usage: 'shuffle',
+      example: ['shuffle'],
+      cooldown: 3,
+      category: 'Music',
+      clientPermissions: ['CONNECT', 'SPEAK'],
+      inVoiceChannel: true,
+      requiredArgs: false,
+    });
+  }
+  async exec(message, args) {
+    const player = this.client.manager.players.get(message.guild.id);
+
+    const channel = message.member.voice.channel;
+
+    const bot = message.guild.me.voice.channel;
+
+    if (!channel)
+      return message.channel.send(
+        '> You need to be in a voice channel to use this command.'
+      );
+
+    if (channel.id !== bot.id)
+      return message.channel.send(
+        '> You are not in the same voice channel as mine to use this command.'
+      );
+
+    if (!player)
+      return message.channel.send('> There is no music playing in the guild.');
+
+    player.queue.shuffle();
+    return message.channel.send('> Shuffled the queue.');
+  }
+};
